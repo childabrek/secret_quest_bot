c1bc8025dfaf7bef9dd804b9aebe6e5f2112761e
CHILLDABREK <childabrek@yandex.ru>
2022-04-06 17:28:03 +0300

    first 1.1.3 clear code

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 import logging
-import asyncio
-from datetime import datetime
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher.filters import Text
-import re
 import psycopg2
-import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode, InputFile
 from aiogram.utils import executor
 from openpyxl import Workbook
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message
 import os
-from aiogram_broadcaster import MessageBroadcaster
+import config
 
 # logs level
 logging.basicConfig(level=logging.INFO)
-DB_URI = 'postgres://afzlxrjvaklsms:2acd0197ef23a3692c85d7fca7922eeed81046ca1597d9f' \
-         '31cb559b243b9f2e8@ec2-79-125-26-60.eu-west-1.compute.amazonaws.com:5432/ddnqvvcb17288e'
+DB_URI = config.DB
 
 # initialization bot
 # TOKEN = your API token
-bot = Bot(token='5295251052:AAG0BkwstG0vN59muiaOIpsUIFHpv-2jIBo')
+bot = Bot(token=config.TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
@@ -58,19 +46,11 @@ class Form(StatesGroup):
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     username = message.from_user.username
-    # db_object.execute(f'SELECT id FROM users WHERE id = {id1}')
-    # result = db_object.fetchone()
-    #
-    # if not result:
-    #     db_object.execute('INSERT INTO users(id, username, name, phone_number) VALUES (%s, %s, %s)',
-    #                       (id1, username, 0, 0))
-    #     db_connection.commit()
     await message.reply(f"Привет! {username}", reply_markup=keyboard)
 
 
 # @dp.message_handler(commands=['referal'])
 # async def referal_start(message: types.Message):
-#     my_channel_id = -1001478296614
 #     user_channel_status = await bot.get_chat_member(chat_id=my_channel_id, user_id=message.chat.id)
 #     user_channel_status = re.findall(r"\w*", str(user_channel_status))
 #     try:
@@ -95,24 +75,11 @@ async def process_start_command(message: types.Message):
 #                 await bot.send_photo(message.from_user.id, f)
 #             await bot.send_message(message.from_user.id, 'ne ok1')
 #             # Условие для тех, кто не подписан
-#
-#
-# @dp.message_handler(Text(equals='Задать вопрос'))
-# async def one_use_answer(message: types.Message):
-#     await message.answer('@siberian_vapor_102')
-#
-#
-# @dp.message_handler(Text(equals='ОДНОРАЗКИ'))
-# async def one_use_answer(message: types.Message):
-#     with open('one_use.txt', 'r', encoding='utf-8') as f:
-#         texts = f.read()
-#         await message.answer(texts)
 
 
 @dp.message_handler(Text(equals='Зарегистрироваться'))
 async def process_start_command2(message: types.Message):
     # Test user for registration yet
-
     db_object.execute(f'SELECT telegram_id FROM users WHERE telegram_id = {message.from_user.id}')
     result = db_object.fetchone()
 
@@ -142,14 +109,6 @@ async def process_name(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=Form.phone_number)
 async def process_number(message: types.Message, state: FSMContext):
-    # a = input()
-    #
-    # d = '1234567890-()+'
-    #
-    # for i in a:
-    #     if i not in d:
-    #         await state.reset_state(True)
-
     async with state.proxy() as data:
         data['phone_number'] = message.text
         phone = data['phone_number']
@@ -206,4 +165,8 @@ async def excel(message: types.Message):
 
 # запускаем лонг поллинг
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    while True:
+        try:
+            executor.start_polling(dp, skip_updates=True)
+        except Exception:
+            pass
